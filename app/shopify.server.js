9443c6fc7cb44782bd19a230935d460d5746f0a3
KelvinMomanyi <kelvinmata42@gmail.com>
2025-04-16 09:41:02 +0300

    new

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -6,7 +6,7 @@ import {
   BillingInterval
 } from "@shopify/shopify-app-remix/server";
 // import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
-import prisma from "./db.server";
+//import prisma from "./db.server";
 
 
 
